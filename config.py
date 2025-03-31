46af8ec54f0e902efbc7e8637eaa84ac21cd5c0b
Panda-Killer <farhan96874@gmail.com>
2025-03-31 15:28:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ MONGO_DB_URI = getenv("mongodb+srv://karan69:karan69@cluster0.gfw7e.mongodb.net/
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
 
 # Chat id of a group for logging bot's activities
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", None))
+LOG_GROUP_ID = int(getenv("-1002147955274", None))
 
 # Get this value from @MissRose_Bot on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", None))
